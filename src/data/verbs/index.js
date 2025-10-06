3f72045ed204d5fb55c5cd52cb265850938a4843
Stef Tapsoba <set89@drexel.edu>
2025-10-06 17:38:15 -0400

    feat(verbs) Added verb Fare and indicativo conjugations. #19

--- src/data/verbs/index.js
+++ src/data/verbs/index.js
@@ -2,5 +2,6 @@ import avere from './avere.js';
 import andare from './andare.js';
 import dire from './dire.js';
 import essere from './essere.js';
+import fare from './fare.js';
 
-export default [essere, avere, andare, dire];
+export default [essere, avere, andare, dire, fare];
