e78ebc175493ca681cb1f32a635ba270b430d081
Stef Tapsoba <set89@drexel.edu>
2025-10-07 20:04:31 -0400

    feat(verbs) Add indicativo for Venire #22

--- src/data/verbs/index.js
+++ src/data/verbs/index.js
@@ -4,5 +4,6 @@ import dire from './dire.js';
 import essere from './essere.js';
 import fare from './fare.js';
 import volere from './volere.js';
+import venire from './venire.js';
 
-export default [essere, avere, andare, dire, fare, volere];
+export default [essere, avere, andare, dire, fare, volere, venire];
